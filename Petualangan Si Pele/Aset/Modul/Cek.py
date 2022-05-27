729e9cee6f093867e950cfb4c5b950f7dbe976f7
Rozin <103322478+MRozinAsySyaddad@users.noreply.github.com>
2022-05-27 13:01:50 +0700

    Update Cek.py

--- Petualangan Si Pele/Aset/Modul/Cek.py
+++ Petualangan Si Pele/Aset/Modul/Cek.py
@@ -1,39 +1,41 @@
-def tampil(teks:str, posisi:str = 'tengah'):
-	if posisi == 'kiri':
-		print('| ' + '{:<40}'.format(teks) + ' |')
-	else:
-		print('| ' + '{:^40}'.format(teks) + ' |')
+from .Konstan import (
+    UKURAN_LAYAR,
+    NAMA,
+    LINK
+)
+
+
+def tampil(teks: str, posisi: str = 'tengah'):
+    if posisi == 'kiri':
+        print('| ' + '{:<40}'.format(teks) + ' |')
+    else:
+        print('| ' + '{:^40}'.format(teks) + ' |')
+
 
 def garis():
-	tampil('-'*40, 'tengah')
+    tampil('-'*40, 'tengah')
 
-from .Menu import Menu_utama as MENU
-
-from .Konstan import (
-	UKURAN_LAYAR, 
-	NAMA, 
-	LINK
-)
-from .Gambar.Icon import ICON
 
 try:
-	import pygame
+    from .Fungsi import TAMPILAN, LAMBANG, JUDUL
+    from .Menu import Menu_utama as MENU
+    from .Gambar.Icon import ICON
 except ModuleNotFoundError:
-	print()
-	garis()
-	tampil('#- Sepele Squad - 04 -#')
-	garis()
-	tampil('Pesan :', 'kiri')
-	tampil('=> Modul Pygame Tidak Ada <=')
-	garis()
-	tampil('Butuh Bantuan ?', 'kiri')
-	garis()
-	masukan = input('=> Pilih (y/n): ')
-	if masukan in ('Y', 'y'):
-		import os
-		os.system('start https://www.pygame.org/wiki/GettingStarted')
-	exit()
+    print()
+    garis()
+    tampil('#- Sepele Squad - 04 -#')
+    garis()
+    tampil('Pesan :', 'kiri')
+    tampil('=> Modul Pygame Tidak Ada <=')
+    garis()
+    tampil('Butuh Bantuan ?', 'kiri')
+    garis()
+    masukan = input('=> Pilih (y/n): ')
+    if masukan in ('Y', 'y'):
+        import os
+        os.system('start https://www.pygame.org/wiki/GettingStarted')
+    exit()
 finally:
-	tampil('Salam Hangat Kami - Sepele Squad (04)')
-	garis()
-	print('LINK :', LINK)
+    tampil('Salam Hangat Kami - Sepele Squad (04)')
+    garis()
+    print('LINK :', LINK)
