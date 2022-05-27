99670d03afcf33ba11a7d718bb7d0f69aa778af1
Rozin <103322478+MRozinAsySyaddad@users.noreply.github.com>
2022-05-27 13:00:48 +0700

    Update Petualangan_Si_Pele.py

--- Petualangan Si Pele/Petualangan_Si_Pele.py
+++ Petualangan Si Pele/Petualangan_Si_Pele.py
@@ -1,8 +1,7 @@
 from Aset.Modul.Cek import *
 
 if __name__ == '__main__':
-	Layar = pygame.display.set_mode(UKURAN_LAYAR)
-	pygame.display.set_icon(ICON.convert())
-	pygame.display.set_caption(NAMA)
-	Menu = Menu_utama()
-	Menu.tampil(Layar)
+    Layar = TAMPILAN(UKURAN_LAYAR)
+    LAMBANG(ICON.convert())
+    JUDUL(NAMA)
+    Menu = MENU.Menu_utama(Layar)
